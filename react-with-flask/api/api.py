943fb84d3afaea006091864752200fdd99c4e223
Quentin FABLE <quentin.fable@ganil.fr>
2025-09-16 09:26:30 +0200

    Update: Dockerize Flask+React with uwsgi and nginx server

--- react-with-flask/api/api.py
+++ react-with-flask/api/api.py
@@ -1,7 +1,13 @@
-import time
 from flask import Flask
+import time
 
-app = Flask(__name__)
+# 'static_folder': tells Flask where the static folder is (by default it is a 'static' directory located in the same directory of the application).
+# 'static_url_path': tells Flask what is the URL prefix for all static files (by default this is '/static').
+app = Flask(__name__, static_folder='../dist', static_url_path='/')
+
+@app.route('/index')
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/api/time')
 def get_current_time():
---
+++ react-with-flask/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from react-with-flask!")
+
+
+if __name__ == "__main__":
+    main()
